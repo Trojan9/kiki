569f42c7c38d0fe21ec6eec9d7290ba815a23857
Trojan9 <timmybanjo@gmail.com>
2021-01-11 18:59:03 +0100

    life is sweet when you get money oo

---
+++ lib/pages/homepage.dart
@@ -0,0 +1,313 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:kiki/productlist.dart';
+import 'package:webscrollbar/webscrollbar.dart';
+
+const int DEFAULT_SCROLL_SPEED = 130;
+const int DEFAULT_NORMAL_SCROLL_ANIMATION_LENGTH_MS = 250;
+
+class Homepage extends StatefulWidget {
+  @override
+  _HomepageState createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  ScrollController controller = ScrollController();
+  final int scrollSpeed = DEFAULT_SCROLL_SPEED;
+
+  final Curve curve = Curves.linear;
+  final int scrollAnimationLength = DEFAULT_NORMAL_SCROLL_ANIMATION_LENGTH_MS;
+  double _scroll = 0;
+  @override
+  Widget build(BuildContext context) {
+    controller.addListener(() {
+      if (controller.position.activity is IdleScrollActivity) {
+        _scroll = controller.position.extentBefore;
+      }
+    });
+    return RawKeyboardListener(
+      focusNode: FocusNode(),
+      onKey: (RawKeyEvent event) {
+        int millis = scrollAnimationLength;
+
+        if (event.isKeyPressed(LogicalKeyboardKey.arrowDown)) {
+          setState(() {
+            _scroll += scrollSpeed;
+          });
+          if (_scroll > controller.position.maxScrollExtent) {
+            _scroll = controller.position.maxScrollExtent;
+            millis = scrollAnimationLength ~/ 2;
+          }
+          if (_scroll < 0) {
+            _scroll = 0;
+            millis = scrollAnimationLength ~/ 2;
+          }
+          controller.animateTo(
+            _scroll,
+            duration: Duration(milliseconds: millis),
+            curve: curve,
+          );
+        }
+        if (event.isKeyPressed(LogicalKeyboardKey.arrowUp)) {
+          setState(() {
+            _scroll -= scrollSpeed;
+          });
+          if (_scroll > controller.position.maxScrollExtent) {
+            _scroll = controller.position.maxScrollExtent;
+            millis = scrollAnimationLength ~/ 2;
+          }
+          if (_scroll < 0) {
+            _scroll = 0;
+            millis = scrollAnimationLength ~/ 2;
+          }
+          controller.animateTo(
+            _scroll,
+            duration: Duration(milliseconds: millis),
+            curve: curve,
+          );
+        }
+        // print(event.logicalKey.keyLabel);
+      },
+      autofocus: true,
+      child: Container(
+        color: Colors.red,
+        child: WebScrollBar(
+          controller: controller,
+          visibleHeight: MediaQuery.of(context).size.height,
+          child: Scaffold(
+            appBar: CustomAppBar(),
+            body: SingleChildScrollView(
+              physics: NeverScrollableScrollPhysics(),
+              controller: controller,
+              child: Container(
+                color: Colors.white,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(top: 18.0, bottom: 18),
+                      child: Align(
+                        alignment: Alignment.center,
+                        child: Column(
+                          children: [
+                            Text("Kiki Collections",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                            Text(
+                              "Shop from our current collections",
+                              style:
+                                  TextStyle(color: Colors.black, fontSize: 18),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: Card(
+                          elevation: 1,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                          child: Image.asset(
+                            "banner.png",
+                            height: MediaQuery.of(context).size.height / 1.5,
+                            // width: MediaQuery.of(context).size.width,
+                          )),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: MediaQuery.of(context).size.width / 8,
+                          right: MediaQuery.of(context).size.width / 8,
+                          top: 15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Align(
+                            alignment: Alignment.topLeft,
+                            child: Text("ACCESSORIES",
+                                style: TextStyle(
+                                  color: Colors.black,
+                                  fontSize: 25,
+                                  fontWeight: FontWeight.bold,
+                                )),
+                          ),
+                          SizedBox(
+                            height: MediaQuery.of(context).size.height / 10,
+                          ),
+                          GridView.count(
+                              crossAxisCount: 3,
+                              //scrollDirection: Axis.vertical,
+                              //itemCount: newdesignreadywearlist.length,
+                              primary: false,
+                              shrinkWrap: true,
+                              physics: NeverScrollableScrollPhysics(),
+                              scrollDirection: Axis.vertical,
+                              childAspectRatio: (2 / 2),
+                              children: List.generate(
+                                12,
+                                (index) {
+                                  return Listproducts();
+                                },
+                              ))
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 6,
+                    ),
+                    Container(
+                      color: Colors.grey[350],
+                      height: MediaQuery.of(context).size.height / 3,
+                      width: MediaQuery.of(context).size.width,
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [Text(""), Text("@Copyright 2020")],
+                        ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class CustomAppBar extends StatefulWidget implements PreferredSizeWidget {
+  CustomAppBar({Key key})
+      : preferredSize = Size.fromHeight(60.0),
+        super(key: key);
+
+  @override
+  final Size preferredSize;
+
+  @override
+  _CustomAppBarState createState() => _CustomAppBarState();
+}
+
+class _CustomAppBarState extends State<CustomAppBar> {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      title: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Expanded(
+              flex: 1,
+              child: Image.asset(
+                "amazonlogo.png",
+                width: 50,
+                height: 50,
+              ),
+            ),
+            Expanded(
+              flex: 4,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "CAMERA",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  Text(
+                    "ACCESSORIES",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  Text(
+                    "SHOES",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  Text(
+                    "WRISTWATCHES",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                  Text(
+                    "PERFUMES",
+                    style: TextStyle(color: Colors.black, fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Row(
+                    children: [
+                      Icon(
+                        Icons.account_circle,
+                        color: Colors.black,
+                      ),
+                      Text(
+                        "Account",
+                        style: TextStyle(color: Colors.black, fontSize: 18),
+                      )
+                    ],
+                  ),
+                  Stack(
+                    children: [
+                      Icon(
+                        Icons.shopping_cart,
+                        size: 25,
+                        color: Colors.black,
+                      ),
+                      Positioned(
+                        right: 0.0,
+                        child: Container(
+                          padding: EdgeInsets.all(1),
+                          decoration: BoxDecoration(
+                            color: Colors.red,
+                            borderRadius: BorderRadius.circular(6),
+                          ),
+                          constraints: BoxConstraints(
+                            minWidth: 13,
+                            minHeight: 13,
+                          ),
+                          child: Padding(
+                            padding: EdgeInsets.only(top: 1),
+                            child: Text(
+                              //mynewCartlength == null ? "0" :
+                              "0",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 8,
+                              ),
+                              textAlign: TextAlign.center,
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      // actions: <Widget>[
+      //   IconButton(
+      //     icon: Icon(
+      //       Icons.add,
+      //       color: Colors.white,
+      //     ),
+      //   ),
+      // ],
+    );
+  }
+}
---
+++ lib/productlist.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Listproducts extends StatefulWidget {
+  @override
+  _ListproductsState createState() => _ListproductsState();
+}
+
+class _ListproductsState extends State<Listproducts> {
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+        shape:
+            RoundedRectangleBorder(borderRadius: BorderRadius.circular(10.0)),
+        elevation: 3.0,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(children: <Widget>[
+            Expanded(
+              flex: 4,
+              child: Image.asset(
+                "product.jpg",
+                height: MediaQuery.of(context).size.height / 2,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Text(
+                    "Ariana Grande Ari Eau de Parfum Spray for Women, 3.4 Fl Oz",
+                    maxLines: 2,
+                  ),
+                  Text(
+                    "\u{20A6}18,000",
+                    maxLines: 2,
+                  )
+                ],
+              ),
+            )
+          ]),
+        ));
+  }
+}
